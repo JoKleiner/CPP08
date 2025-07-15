674bd86063bd61d52210839693addf690e69da97
joklein <joklein@student.42heilbronn.de>
2025-07-15 14:46:25 +0200

    change some norm

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: joklein <joklein@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 13:00:01 by joklein           #+#    #+#             */
-/*   Updated: 2025/07/15 14:42:17 by joklein          ###   ########.fr       */
+/*   Updated: 2025/07/15 14:46:13 by joklein          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,10 @@ unsigned int Span::shortestSpan(){
     auto sort = m_array; 
     std::sort(sort.begin(), sort.end());
     int short_span = sort[1] - sort[0];
-    for(size_t i = 1; i < sort.size(); i++)
+    for(size_t i = 1; i < sort.size(); i++){
         if(sort[i] - sort[i-1] < short_span)
             short_span = sort[i] - sort[i-1];
+    }
     return(short_span);
 }
 
@@ -49,16 +50,16 @@ unsigned int Span::longestSpan(){
 }
 
 void Span::add_multiple_num(unsigned int num){
-
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(INT_MIN, INT_MAX);
     std::vector<int> temp_array;
     
-    for(size_t i = 0; i < num; i++)
+    for(size_t i = 0; i < num; i++){
         temp_array.push_back(dist(gen));
-    
-    if(this->m_size < this->m_array.size() + temp_array.size())
+    }
+    if(this->m_size < this->m_array.size() + temp_array.size()){
         throw(std::length_error("Out of bounds"));
+    }
     this->m_array.insert(this->m_array.end(), temp_array.begin(), temp_array.end());
 }
